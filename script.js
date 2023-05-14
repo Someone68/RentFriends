beae370bb65cc7226f0e459dfa16f9f05f091727
ducksquatarmy <unconfigured@null.spigotmc.org>
2023-05-14 15:25:34 -0700

    massive update

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ let interest = document.getElementById("interest");
 let price = document.getElementById("price");
 
 function calcu(){
-    let total = eval(stay.value) + eval(know.value) + eval(loyalty.value) + eval(interest.value);
+    let total = eval(stay.value) * 10 + eval(know.value) + eval(loyalty.value) + eval(interest.value) - 0.01;
     price.innerHTML = "Total: $ "+total;
 }
